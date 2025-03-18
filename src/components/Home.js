421afd2bc03c8e61439a4f2d1b28dd2136dfb89d
fatimaa-ahmad <fatimahmad2222@gmail.com>
2025-03-19 01:14:17 +0500

    Updated homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
       <h2>Welcome</h2>
       <p>Welcome to our fabulous flower and chocolate vending machine service. Enjoy a world of beauty and sweetness—available 24/7!</p>
       <img 
-        src="/vending.jpg" 
+        src="/vending.png" 
         alt="Vending Machine"
         className="homepage-image" // This applies the unique class for styling
       />
